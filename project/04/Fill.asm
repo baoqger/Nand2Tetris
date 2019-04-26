6f867fe2dfefb8d7a7ead5c09e74d071d8667629
Jingjing Bao <jingjing.bao@grabtaxi.com>
2019-04-26 21:56:37 +0800

    04

---
+++ project/04/Fill.asm
@@ -0,0 +1,61 @@
+// Program: Rectangle.asm
+// Draws a filled rectangle at the
+// screen's top left corner, with
+// width of 16 pixels and height of
+// RAM[0] pixels.
+// Usage: put a non-negative number
+// (rectangle's height) in RAM[0].
+(LOOP)
+@SCREEN
+D=A
+@scr
+M=D      // scr = SCREEN
+@KBD
+D=A
+@kbd
+M=D      // kbd = KBD
+
+@KBD
+D=M
+@BLACK
+D;JNE
+@WHITE
+D;JEQ
+
+@LOOP
+0;JMP 
+
+(BLACK)
+@scr
+D=M
+@kbd
+D=D-M
+@LOOP
+D;JGE // if scr >= kbd goto LOOP
+
+@scr
+A=M
+M=-1      // RAM[src]=11111111
+
+@scr
+M=M+1
+@BLACK
+0;JMP
+
+(WHITE)
+@scr
+D=M
+@kbd
+D=D-M
+@LOOP
+D;JGE // if scr >= kbd goto LOOP
+
+@scr
+A=M
+M=0      // RAM[src]=00000000
+
+@scr
+M=M+1
+@WHITE
+0;JMP
+
---
+++ project/04/Mult.asm
@@ -0,0 +1,50 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+
+// Put your code here.
+// Computes RAM[1] = 1+2+ ... +n
+// Usage: put a number (n) in RAM[0]
+@R0
+D=M
+@n
+M=D // n = R0
+@R1
+D=M
+@m
+M=D // m = R1
+@i
+M=1 // i = 1
+@sum
+M=0 // sum = 0
+@R2
+M=0
+(LOOP)
+@i
+D=M
+@n
+D=D-M
+@STOP
+D;JGT // if i > n goto STOP
+@sum
+D=M
+@m
+D=D+M
+@sum
+M=D // sum = sum + m
+@i
+M=M+1 // i = i + 1
+@LOOP
+0;JMP
+(STOP)
+@sum
+D=M
+@R2
+M=D // RAM[2] = sum
+(END)
+@END
+0;JMP 
